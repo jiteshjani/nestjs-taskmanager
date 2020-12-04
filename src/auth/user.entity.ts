36f795368a3d70a2e011d7773d9281b23fa2e9e3
Jitesh Jani <jani.jitesh@gmail.com>
2020-12-04 18:04:12 +0530

    Creates relationship between user and tasks

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -2,10 +2,12 @@ import {
   BaseEntity,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
 import * as argon2 from 'argon2';
+import { Task } from 'src/tasks/task.entity';
 
 @Entity()
 @Unique(['username'])
@@ -18,6 +20,9 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  @OneToMany((type) => Task, (task) => task.user, { eager: true })
+  tasks: Task[];
 
   async validatePassword(password: string): Promise<boolean> {
     try {
