fc1f4c2d1a6aa4e29a731c6ba3866e16e77802b2
Jitesh Jani <jani.jitesh@gmail.com>
2020-12-07 22:44:51 +0530

    Implements ServeStaticModule for serving static assets

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 
+const rootPath = join(__dirname, '..', 'client');
+console.log(rootPath);
+
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule, AuthModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath,
+      exclude: ['/api*'],
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TasksModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
 })
 export class AppModule {}
