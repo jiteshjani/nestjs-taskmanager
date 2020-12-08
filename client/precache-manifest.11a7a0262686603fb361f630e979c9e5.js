39c287d16623e64edc5d3227bb80b84d5ce7819b
Jitesh Jani <jani.jitesh@gmail.com>
2020-12-08 13:06:57 +0530

    Adds compiled react front end task managment

---
+++ client/precache-manifest.11a7a0262686603fb361f630e979c9e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f561fa1382ce0f860929d42c3a44d3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "021fb0d621daac0a6a45",
+    "url": "/static/css/main.1dc3eda2.chunk.css"
+  },
+  {
+    "revision": "87ef5a170cccd219e061",
+    "url": "/static/js/2.fe2e667d.chunk.js"
+  },
+  {
+    "revision": "021fb0d621daac0a6a45",
+    "url": "/static/js/main.58cb6ce1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
